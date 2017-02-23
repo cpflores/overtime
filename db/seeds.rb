fa0a80bb7779c53f1052cfa996c1a3ba3f5c79f5
Chadwick Flores <chadwickflores@gmail.com>
2017-02-22 19:40:33 -0600

    built out rake task for sending sms messages and STI for employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-@user = User.create(email: "test@test.com",
+@employee = Employee.create(email: "test@test.com",
                     password: "asdfasdf",
                     password_confirmation: "asdfasdf",
-                    first_name: "Jon",
-                    last_name: "Snow",
+                    first_name: "Scott",
+                    last_name: "Summers",
                     phone: "4143644544")
-puts "1 User created"
+puts "1 employee created"
 
 AdminUser.create(email: "admin@test.com",
                  password: "asdfasdf",
@@ -21,16 +21,16 @@ AdminUser.create(email: "admin@test.com",
                  phone: "4143644544")
 puts "1 Admin User created"
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
 
 100.times do |post|
   Post.create!(date: Date.today,
                rationale: "#{post} rationale content Lomo tattooed tilde, ramps hot chicken hashtag squid. Chicharrones XOXO cold-pressed, flexitarian paleo authentic celiac pickled kickstarter neutra coloring book flannel. Ugh fashion axe seitan put a bird on it, flannel raw denim jean shorts selfies. Direct trade literally intelligentsia, kinfolk crucifix aesthetic squid health goth etsy. Tote bag jean shorts fam letterpress pitchfork, subway tile scenester disrupt dreamcatcher plaid retro vaporware photo booth ethical bushwick. Tousled pabst distillery mustache letterpress taxidermy. Sartorial lyft four loko, farm-to-table disrupt unicorn pug man braid selvage tofu.",
-               user_id: @user.id,
+               user_id: @employee.id,
                overtime_request: 2.5)
 end
 
